4869b41dbd0fadb348b43762c364518e5ab4edf8
DaviMottaa <84351196+DaviMottaa@users.noreply.github.com>
2021-06-11 18:11:25 -0300

    Add files via upload

---
+++ Davi_10621.py
@@ -0,0 +1,52 @@
+
+# Qustion 1
+Lista = ["Arroz", "Feijão", "Macarrão", "Maçã", "Banana", "Mixirica", "Laranja"]
+print(Lista)
+
+#Question2
+Lista2 = Lista[0]+" "+Lista[1]
+
+# Question4
+print(Lista2)
+
+# Question5
+print(Lista[-6])
+
+# Question6
+Lista3 = Lista[2]
+
+#Question7
+print(Lista3.upper())
+
+#Question8
+Lista.insert(3, "Lasanha")
+
+#Question9
+print(Lista)
+
+#Question10
+del Lista[2]
+
+#Question11
+print(Lista)
+
+#Question12
+item = Lista.pop(-0)
+print(Lista)
+
+#Question13
+print(Lista)
+print(item)
+
+#Question14
+item2 = Lista.pop(0)
+
+#Question15
+print(Lista)
+print(item2)
+
+#Question16
+Lista.sort()
+
+#Question17
+print(Lista)
